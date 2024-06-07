cc6a00f48a4a82779065e84d6cc3abb870d88702
Delfics <Soulspirit95@gmail.com>
2024-06-07 10:31:06 +0300

    Тз 10 выполнено поправил работу от Ревьюера

--- src/test/java/ru/yandex/practicum/filmorate/service/ValidateUserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ValidateUserTest.java
@@ -1,14 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
