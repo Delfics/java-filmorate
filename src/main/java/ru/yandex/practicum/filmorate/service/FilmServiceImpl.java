ef0722e25b871111e044229f9ec25483de89436e
Delfics <Soulspirit95@gmail.com>
2024-08-06 20:03:30 +0300

    Тз 12 коммит, поправил CheckStyle4

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dao.user.UserStorage;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.*;
 import ru.yandex.practicum.filmorate.dao.film.FilmStorage;
